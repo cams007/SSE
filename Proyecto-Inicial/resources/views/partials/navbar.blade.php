970b5ad3120785d8987ea189ba48a2e1695ed034
adrian <adri.castillo18@gmail.com>
2017-05-25 01:04:30 -0500

    Se agrega enlace de ranking y tabulador a navbar.

--- Proyecto-Inicial/resources/views/partials/navbar.blade.php
+++ Proyecto-Inicial/resources/views/partials/navbar.blade.php
@@ -1,8 +1,8 @@
 
           <ul id="menu">
-            <li class="item"><a href="#">Ofertas laborales</a></li>
+            <li class="item"><a href="{{url('ofertas')}}">Ofertas laborales</a></li>
             <li class="item"><a href="#">Directorio de empresas</a></li>
-            <li class="item"><a href="#">Ranking de empresas</a></li>
+            <li class="item"><a href="{{url('ranking')}}">Ranking de empresas</a></li>
             <li class="item"><a href="#">Tabulador de salarios</a></li>
             <li class="item"><a href="#">Eventos UTM</a></li>
             <li class="item"><a href="#">Historias de éxito</a></li>
