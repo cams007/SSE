c71c5939836d376de79fd687350bf2745a4ce278
adrian <adri.castillo18@gmail.com>
2017-05-31 04:53:44 -0500

    Add view comentarios, ofertasLaborales, index (empresa) y asideEmpresa

---
+++ Proyecto-Inicial/resources/views/empresa/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.master')
+
+@section('title', 'Datos basicos')
+
+@section('style')
+<link href="{{ url('css/perfil.css') }}" rel="stylesheet">
+@stop
+
+@section('content')
+	<h1 class="text-center">Datos de empresa</h1>
+	<hr class="hr">
+	<div class="clearfix">
+		<aside id="cssmenu" class="column hrV">
+			@include('partials.asideEmpresa')
+		</aside>
+		
+		<div class="column content-sm">
+			<form method="POST" action="#">
+			
+				{{-- TODO: Protección contra CSRF --}}
+				{{ csrf_field() }}
+
+				<div>	
+					<img src="{{ url('assets/images/address.png') }}" alt="" class="iconos"> 
+					<span> {{" Apple Inc. "}} </span>
+				</div>
+
+				<div>
+					<img src="{{ url('assets/images/home0.png') }}" alt="" class="iconos"> 
+					<span> {{" Cupertino, California, Estados Unidos "}} </span>
+				</div>
+
+				<div>
+					<img src="{{ url('assets/images/phone.png') }}" alt="" class="iconos"> 
+					<span> {{" 1-800-275-2273 "}} </span>
+				</div>
+
+				<div>
+					<img src="{{ url('assets/images/email.png') }}" alt="" class="iconos"> 
+					<span> {{" info@apple.com "}} </span>
+				</div>
+
+				<div>
+					<img src="{{ url('assets/images/user0.png') }}" alt="" class="iconos"> 
+					<span> {{" Tim Cook "}} </span>
+				</div>
+
+				<div>
+					<img src="{{ url('assets/images/email.png') }}" alt="" class="iconos"> 
+					<span> {{" CEO "}} </span>
+				</div>
+
+				<div class="input tel">
+					<img src="{{ url('assets/images/start_noselect.png') }}" alt="" class="iconos"> 
+					<label class="link"> {{" Calificar empresa "}}</label>
+				</div>
+
+				
+			</form>
+		</div>
+	
+		<div class="column">
+	    	<img src="{{url('assets/images/logo_utm.png')}}" alt="user-picture" class="img-thumbnail img">
+	    	<div>
+	    		<br>
+	    		<h3>3.2</h3>
+		    	<img src="{{ url('assets/images/start_select.png') }}">
+				<img src="{{ url('assets/images/start_select.png') }}">
+				<img src="{{ url('assets/images/start_select.png') }}">
+				<img src="{{ url('assets/images/start_select.png') }}">
+				<img src="{{ url('assets/images/start_noselect.png') }}">
+				<h6>12 evaluaciones</h6>
+			</div>
+		</div>
+	</div>
+@stop
