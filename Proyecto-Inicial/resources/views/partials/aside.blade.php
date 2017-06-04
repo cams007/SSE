4b917fe099985f3ef6ce56af421eee9e606ea800
Carlos Villalvazo <zentrix.24@gmail.com>
2017-06-03 19:23:25 -0500

    Add SASS Support

--- Proyecto-Inicial/resources/views/partials/aside.blade.php
+++ Proyecto-Inicial/resources/views/partials/aside.blade.php
@@ -1,7 +1,7 @@
 
 <ul style="border: 1px outline ">
 	  <li {{ Request::is('perfil') ? ' class=active' : ''}} >
-	  	<a href="{{url('perfil')}}">Datos basicos</a>
+	  	<a href="{{url('perfil')}}">Datos básicos</a>
 	  </li>
 	  <li {{ Request::is('perfil/fpersonal') ? ' class=active' : ''}}>
 	  	<a href="{{url('perfil/fpersonal')}}">Estudios realizados</a>
@@ -18,10 +18,10 @@
 	  <li {{ Request::is('perfil/egresadoReco') ? ' class=active' : ''}}>
 	  	<a href="{{url('perfil/egresadoReco')}}"> Recomendaciones egresado</a>
 	  </li>
-	  <li {{ Request::is('perfil/intereses') ? ' class=active' : ''}}>
+	  <!-- <li {{ Request::is('perfil/intereses') ? ' class=active' : ''}}>
 	  	<a href="{{url('perfil/intereses')}}">Mis intereses</a>
 	  </li>
 	  <li {{ Request::is('perfil/ofertaslab') ? ' class=active' : ''}}>
 	  	<a href="{{url('perfil/ofertaslab')}}">Mis ofertas laborales</a>
-	  </li>
+	  </li> -->
 </ul>
