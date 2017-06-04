c3c2eccbbe59c544478935843b80bb645f3b467d
ccmarcos <marcosantonio199189@gmail.com>
2017-06-04 12:11:48 -0500

    agregando paginacion

--- Proyecto-Inicial/resources/views/egresados/eventosUTM/index.blade.php
+++ Proyecto-Inicial/resources/views/egresados/eventosUTM/index.blade.php
@@ -8,172 +8,128 @@
 @stop
 
 @section('content')
-<<<<<<< HEAD
-	<h1 class="text-center">Eventos UTM</h1>
-	<hr class="hr">
-	
-	<div class="clearfix">
-		<aside class="column" id="cssmenu">
-			@include('partials.AsideEventosUtm')	
-		</aside>
-		<div class="column content">
-			<div class="filtro_3">
-				<input type="text" name="busquedaEvento" value="busqueda de evento" class="inputBusqueda">
-			</div>
+<div class="contenedor"><!--inicio contenedor-->
 
-			<div>
-				<figure>
-	 				<img src="{{ url('assets/images/acad1UTM.PNG') }}" width="400" height="353">
-					<figcaption><h2>Seminario EQUIPAR</h2></figcaption>
-				</figure>
-				<img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
-				<h3>Martes 07 de Junio de 2017, 10:00 hrs</h3><!--Fecha-->
+ <div class="div-1"> <!--incio div-1-->
 
-				<img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
-				<h3>Auditorio UTM. Huajuapan de león Oaxaca</h3><!--Ubicacion-->
-			</div>
+	 <p class="text-center">Eventos UTM</p>
+	 <hr class="hr">
 
-			<div>
-				<figure>
-	 				<img src="{{ url('assets/images/biblioteca.JPG') }}" width="400" height="353">
-					<figcaption><h2>Orquesta de Oaxaca</h2></figcaption>
-				</figure>
-				<img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
-				<h3>Lunes 25 de enero 2017, 12:00 hrs</h3><!--Fecha-->
+ </div><!--fin div-1-->
 
-				<img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
-				<h3>Auditorio UTM. Huajuapan de león Oaxaca</h3><!--Ubicacion-->
-			</div>
-		</div>
-	</div>
-	<!-- Paginación -->
-	<div class="paginate">
-		<a class="back" href="#"><img src="{{ url('assets/images/paginator_back.png') }}"></a>
-      	<a class="page" href="#">1</a>
-      	<a class="active" href="#">2</a>
-      	<a class="page" href="#">3</a>
-      	<a class="page" href="#">4</a>
-      	<a class="page" href="#">5</a>
-      	<a class="forward" href="#"><img src="{{ url('assets/images/paginator_forward.png') }}"></a>
-	</div>
+ <div class="div-2"><!--inicio div-2-->
 
+	 <div class="div-2-1"><!--inicio div-2-1-->
 
+		 <!--<div class="clearfix">-->
+		 <aside class="column" id="cssmenu">
+			 @include('partials.AsideEventosUtm')
+		 </aside>
 
-@stop
-=======
- <div class="contenedor"><!--inicio contenedor-->
+	 </div><!--fin div-2-1-->
 
-	<div class="div-1"> <!--incio div-1-->
+	 <div class="div-2-2"><!--inicio div-2-2-->
+		 <!--<div class="column content">-->
+		 <div class="div-2-2-1"> <!--inicio div-2-2-1-->
 
-		<p class="text-center">Eventos UTM</p>
-		<hr class="hr">
+			 <div class="filtro_3">
+				 <input type="text" name="busquedaEvento" value="busqueda de evento" class="inputBusqueda">
+			 </div>
 
-	</div><!--fin div-1-->
+		 </div><!--fin div-2-2-1-->
 
-	<div class="div-2"><!--inicio div-2-->
+		 <div class="div-2-2-2"><!--inicio div-2-2-2-->
 
-		<div class="div-2-1"><!--inicio div-2-1-->
+			 <div class="div-2-2-2-1"><!--inicio div2-2-2-1-->
 
-			<!--<div class="clearfix">-->
-			<aside class="column" id="cssmenu">
-				@include('partials.AsideEventosUtm')
-			</aside>
+					 <div class="foto1"><!--incio foto-->
+				 <!--<figure>
+					 <img src="{{ url('assets/images/acad1UTM.PNG') }}" width="400" height="353">
+					 <figcaption><h2>Seminario EQUIPAR</h2></figcaption>
+				 </figure>-->
+					 </div><!--fin foto-->
 
-		</div><!--fin div-2-1-->
+				 <div class="descripcion-foto1"><!--inicio descripcion-foto-->
+					 <div class="texto-descripcion"><!--inicio orquesta-div-1-->
+						 <h2>Orquesta de Oaxaca</h2>
+					 </div><!--fin orquesta-div-1-->
+					 <div class="fecha"><!--inicio fecha-div-2-->
+						 <img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
+						 <h3>Martes 07 de Junio de 2017, 10:00 hrs</h3>
 
-		<div class="div-2-2"><!--inicio div-2-2-->
-			<!--<div class="column content">-->
-			<div class="div-2-2-1"> <!--inicio div-2-2-1-->
+						 <img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
+						 <h3>Auditorio UTM. Huajuapan de león Oaxaca</h3>
+					 </div><!--fin fecha-div-2-->
+					 <div class="asistiras"><!--inicio foto1-asistiras-->
+						 <h3>¿Asistirás?
+					 </div><!--fin foto1-asistiras-->
 
-				<div class="filtro_3">
-					<input type="text" name="busquedaEvento" value="busqueda de evento" class="inputBusqueda">
-				</div>
+					 <div class="botones-si-no">
+						 <input type="button" class="si" value="Sí">
+						 <input type="button" class="no" value="No">
+					 </div>
+				 </div><!--fin descripcion-foto-->
 
-			</div><!--fin div-2-2-1-->
+			 </div><!--fin div-2-2-2-1-->
 
-			<div class="div-2-2-2"><!--inicio div-2-2-2-->
+			 <div class="div-2-2-2-2"><!--inicio div-2-2-2-2-->
 
-				<div class="div-2-2-2-1"><!--inicio div2-2-2-1-->
+				 <div class="foto2"><!--inicio foto2-->
+					 <!--<figure>
+					 <img src="{{ url('assets/images/biblioteca.JPG') }}" width="400" height="353">
+					 <figcaption><h2>Orquesta de Oaxaca</h2></figcaption>
+				 </figure>-->
+				 </div><!--fin foto2-->
 
-						<div class="foto1"><!--incio foto-->
-					<!--<figure>
-	 					<img src="{{ url('assets/images/acad1UTM.PNG') }}" width="400" height="353">
-						<figcaption><h2>Seminario EQUIPAR</h2></figcaption>
-					</figure>-->
-						</div><!--fin foto-->
+				 <div class="descripcion-foto2"><!--inicio descripcion-foto2-->
+					 <div class="texto-descripcion"><!--inicio jarabe-->
+					 <h2>Jarabe Mixteco</h2>
+				 </div><!--fin jarabe-->
+				 <div class="fecha"><!--inicio fecha-jarabe-->
+					 <img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
+					 <h3>Lunes 25 de enero 2017, 12:00 hrs</h3><!--Fecha-->
 
-					<div class="descripcion-foto1"><!--inicio descripcion-foto-->
-						<div class="texto-descripcion"><!--inicio orquesta-div-1-->
-							<h2>Orquesta de Oaxaca</h2>
-						</div><!--fin orquesta-div-1-->
-						<div class="fecha"><!--inicio fecha-div-2-->
-							<img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
-							<h3>Martes 07 de Junio de 2017, 10:00 hrs</h3>
+					 <img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
+					 <h3>Auditorio UTM. Huajuapan de león Oaxaca</h3><!--Ubicacion-->
+				 </div><!--fin fecha-jarabe-->
 
-							<img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
-							<h3>Auditorio UTM. Huajuapan de león Oaxaca</h3>
-						</div><!--fin fecha-div-2-->
-						<div class="asistiras"><!--inicio foto1-asistiras-->
-							<h3>¿Asistirás?
-						</div><!--fin foto1-asistiras-->
+				 <div class="asistiras"><!--inicio foto1-asistiras-->
+					 <h3>¿Asistirás?
+				 </div><!--fin foto1-asistiras-->
 
-						<div class="botones-si-no">
-							<input type="button" class="si" value="Sí">
-							<input type="button" class="no" value="No">
-						</div>
-					</div><!--fin descripcion-foto-->
+				 <div class="botones-si-no">
+					 <input type="button" class="si" value="Sí">
+					 <input type="button" class="no" value="No">
+				 </div>
 
-				</div><!--fin div-2-2-2-1-->
+				 </div><!--fin descripcion-foto2-->
 
-				<div class="div-2-2-2-2"><!--inicio div-2-2-2-2-->
+			 </div><!--fin div-2-2-2-2--->
 
-					<div class="foto2"><!--inicio foto2-->
-						<!--<figure>
-	 					<img src="{{ url('assets/images/biblioteca.JPG') }}" width="400" height="353">
-						<figcaption><h2>Orquesta de Oaxaca</h2></figcaption>
-					</figure>-->
-					</div><!--fin foto2-->
+		 </div><!--fin div-2-2-3-->
 
-					<div class="descripcion-foto2"><!--inicio descripcion-foto2-->
-						<div class="texto-descripcion"><!--inicio jarabe-->
-						<h2>Jarabe Mixteco</h2>
-					</div><!--fin jarabe-->
-					<div class="fecha"><!--inicio fecha-jarabe-->
-						<img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
-						<h3>Lunes 25 de enero 2017, 12:00 hrs</h3><!--Fecha-->
+	 <div class="div-2-2-3"><!--inicio div-2-2-3-->
 
-						<img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
-						<h3>Auditorio UTM. Huajuapan de león Oaxaca</h3><!--Ubicacion-->
-					</div><!--fin fecha-jarabe-->
+		 <div class="paginate">
+	 		<a class="back" href="#"><img src="{{ url('assets/images/paginator_back.png') }}"></a>
+	       	<a class="page" href="#">1</a>
+	       	<a class="active" href="#">2</a>
+	       	<a class="page" href="#">3</a>
+	       	<a class="page" href="#">4</a>
+	       	<a class="page" href="#">5</a>
+	       	<a class="forward" href="#"><img src="{{ url('assets/images/paginator_forward.png') }}"></a>
+	 	</div>
 
-					<div class="asistiras"><!--inicio foto1-asistiras-->
-						<h3>¿Asistirás?
-					</div><!--fin foto1-asistiras-->
+	 </div><!--fin div-2-2-3-->
 
-					<div class="botones-si-no">
-						<input type="button" class="si" value="Sí">
-						<input type="button" class="no" value="No">
-					</div>
+	 <!--</div>-->
 
-					</div><!--fin descripcion-foto2-->
-
-				</div><!--fin div-2-2-2-2--->
-
-			</div><!--fin div-2-2-3-->
-
-		<div class="div-2-2-3"><!--inicio div-2-2-3-->
-
-			<strong>Radio Buttons</strong>
-
-		</div><!--fin div-2-2-3-->
-
-		<!--</div>-->
-
-	</div><!--fin div-2-2-->
-	<!--</div>-->
-	</div><!--fin div-2-->
+ </div><!--fin div-2-2-->
+ <!--</div>-->
+ </div><!--fin div-2-->
 
 </div><!--fin contenedor-->
 
+
+
 @stop
->>>>>>> marcos
