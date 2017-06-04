a68acad004b1de73cdde45c9d09ab78c1051cddf
ccmarcos <marcosantonio199189@gmail.com>
2017-06-04 11:54:25 -0500

    maquetado eventosUTM

--- Proyecto-Inicial/resources/views/egresados/eventosUTM/index.blade.php
+++ Proyecto-Inicial/resources/views/egresados/eventosUTM/index.blade.php
@@ -34,7 +34,7 @@
 				<div class="filtro_3">
 					<input type="text" name="busquedaEvento" value="busqueda de evento" class="inputBusqueda">
 				</div>
-				
+
 			</div><!--fin div-2-2-1-->
 
 			<div class="div-2-2-2"><!--inicio div-2-2-2-->
@@ -49,13 +49,24 @@
 						</div><!--fin foto-->
 
 					<div class="descripcion-foto1"><!--inicio descripcion-foto-->
+						<div class="texto-descripcion"><!--inicio orquesta-div-1-->
+							<h2>Orquesta de Oaxaca</h2>
+						</div><!--fin orquesta-div-1-->
+						<div class="fecha"><!--inicio fecha-div-2-->
+							<img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
+							<h3>Martes 07 de Junio de 2017, 10:00 hrs</h3>
 
-						<img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
-						<h3>Martes 07 de Junio de 2017, 10:00 hrs</h3>
+							<img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
+							<h3>Auditorio UTM. Huajuapan de león Oaxaca</h3>
+						</div><!--fin fecha-div-2-->
+						<div class="asistiras"><!--inicio foto1-asistiras-->
+							<h3>¿Asistirás?
+						</div><!--fin foto1-asistiras-->
 
-						<img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
-						<h3>Auditorio UTM. Huajuapan de león Oaxaca</h3>
-
+						<div class="botones-si-no">
+							<input type="button" class="si" value="Sí">
+							<input type="button" class="no" value="No">
+						</div>
 					</div><!--fin descripcion-foto-->
 
 				</div><!--fin div-2-2-2-1-->
@@ -70,12 +81,25 @@
 					</div><!--fin foto2-->
 
 					<div class="descripcion-foto2"><!--inicio descripcion-foto2-->
-
+						<div class="texto-descripcion"><!--inicio jarabe-->
+						<h2>Jarabe Mixteco</h2>
+					</div><!--fin jarabe-->
+					<div class="fecha"><!--inicio fecha-jarabe-->
 						<img src="{{ url('assets/images/eventos_calendar.png') }}" alt="" class="iconos">
 						<h3>Lunes 25 de enero 2017, 12:00 hrs</h3><!--Fecha-->
 
 						<img src="{{ url('assets/images/eventos_location.png') }}" alt="" class="iconos">
 						<h3>Auditorio UTM. Huajuapan de león Oaxaca</h3><!--Ubicacion-->
+					</div><!--fin fecha-jarabe-->
+
+					<div class="asistiras"><!--inicio foto1-asistiras-->
+						<h3>¿Asistirás?
+					</div><!--fin foto1-asistiras-->
+
+					<div class="botones-si-no">
+						<input type="button" class="si" value="Sí">
+						<input type="button" class="no" value="No">
+					</div>
 
 					</div><!--fin descripcion-foto2-->
 
