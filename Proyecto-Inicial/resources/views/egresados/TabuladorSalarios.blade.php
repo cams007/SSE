0702f48b9a710b4270d2fbdab7dd75d2eee3585d
Adrian <adri.castillo18@gmail.com>
2017-06-09 18:26:05 -0500

    Se corrigen errores de diseño

--- Proyecto-Inicial/resources/views/egresados/TabuladorSalarios.blade.php
+++ Proyecto-Inicial/resources/views/egresados/TabuladorSalarios.blade.php
@@ -3,101 +3,107 @@
 @section('title', 'Tabulador de salarios')
 
 @section('style')
-
-  <link href="{{ url('css/tabuladorSalarios.css') }}" rel="stylesheet">
-
+	<link href="{{ url('css/tabuladorSalarios.css') }}" rel="stylesheet">
 	<link href="{{ url('css/perfil.css') }}" rel="stylesheet">
 	<link href="{{ url('css/ofertas.css') }}" rel="stylesheet">
+	<link href="{{ url('css/table.css') }}" rel="stylesheet">
 @stop
-
 
 @section('content')
 
-	<h1 class="text-center">Tabulador de salarios</h1>
-	<hr class="hr">
-	<!--Filtro-->
-	<div class="filtro_1">
-		<p class="titulo_select1">Carrera:</p>
-		<select name="carrera">
-			<option value="*">Todas</option>
-			<option value="ingAlimentos">Ing. en Alimentos</option>
-			<option value="ingAlimentos">Ing. en Computación</option>
-			<option value="ingAlimentos">Ing. en Diseño</option>
-		</select>
-	</div>
-	<div class="filtro_3"> 							<!--cambiar la clase -->
-		<p class="titulo_select">Busqueda:</p>
-		<input type="text" name="BusquedaEspecifica" value="x">
-	</div>
-	<!--Tabla de resultados-->
-	<table>
-		<tr>
-		  <th>Título del empleo</th>
-		  <th>Años de experiencia</th>
-		  <th>Monto mínimo</th>
-		  <th>Monto máximo</th>
-		</tr>
+	<div class="contenedor"><!--contenedor-->
+	
+		<div class="div-1"><!--div-1-->
+			<p class="text-center">Tabulador de salarios</p>
+	      	<hr class="hr">
+		</div><!--div-1-->
 
-		<tr>
-		  <td>Ingeniero Mecatrónico</td>
-		  <td>2 años</td>
-		  <td>8,000 mensuales</td>
-		  <td>15,000 mensuales</td>
-		</tr>
+		<!--Filtro-->
+		<div class="filtro_1">
+			<p class="titulo_select1">Carrera:</p>
+			<select name="carrera">
+				<option value="*">Todas</option>
+				<option value="ingAlimentos">Ing. en Alimentos</option>
+				<option value="ingAlimentos">Ing. en Computación</option>
+				<option value="ingAlimentos">Ing. en Diseño</option>
+			</select>
+		</div>
 
-		<tr>
-		  <td>Gerente de banco</td>
-		  <td>3 años</td>
-		  <td>20,000 mensuales</td>
-		  <td>30,000 mensuales</td>
-		</tr>
+		<div class="buscador">
+            <input type="search" name="q" placeholder="Buscar">
+        </div>
 
-		<tr>
-		  <td>Administrador de Base de Datos</td>
-		  <td>5 años</td>
-		  <td>20,000 mensuales</td>
-		  <td>30,000 mensuales</td>
-		</tr>
-		<tr>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		</tr>
-		<tr>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		</tr>
-		<tr>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		</tr>
-		<tr>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		</tr>
-		<tr>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		  <td></td>
-		</tr>
-	</table>
+		<!--Tabla de resultados-->
+		<table>
+			<tr>
+			  <th>Título del empleo</th>
+			  <th>Años de experiencia</th>
+			  <th>Monto mínimo</th>
+			  <th>Monto máximo</th>
+			</tr>
 
-	<div class="paginate">
-		<a class="back" href="#"><img src="{{ url('assets/images/paginator_back.png') }}"></a>
-      	<a class="page" href="#">1</a>
-      	<a class="active" href="#">2</a>
-      	<a class="page" href="#">3</a>
-      	<a class="page" href="#">4</a>
-      	<a class="page" href="#">5</a>
-      	<a class="forward" href="#"><img src="{{ url('assets/images/paginator_forward.png') }}"></a>
-	</div>
+			<tr>
+			  <td>Ingeniero Mecatrónico</td>
+			  <td>2 años</td>
+			  <td>8,000 mensuales</td>
+			  <td>15,000 mensuales</td>
+			</tr>
+
+			<tr>
+			  <td>Gerente de banco</td>
+			  <td>3 años</td>
+			  <td>20,000 mensuales</td>
+			  <td>30,000 mensuales</td>
+			</tr>
+
+			<tr>
+			  <td>Administrador de Base de Datos</td>
+			  <td>5 años</td>
+			  <td>20,000 mensuales</td>
+			  <td>30,000 mensuales</td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			</tr>
+		</table>
+
+		<!-- Paginación -->
+		<div class="paginate">
+			<a class="back" href="#"><img src="{{ url('assets/images/paginator_back.png') }}"></a>
+	      	<a class="page" href="#">1</a>
+	      	<a class="active" href="#">2</a>
+	      	<a class="page" href="#">3</a>
+	      	<a class="page" href="#">4</a>
+	      	<a class="page" href="#">5</a>
+	      	<a class="forward" href="#"><img src="{{ url('assets/images/paginator_forward.png') }}"></a>
+		</div>
+	</div><!--contenedor-->
 @stop
 
--- Proyecto-Inicial/resources/views/perfil/fProfesional_propuesta.blade.php
+++ Proyecto-Inicial/resources/views/perfil/fProfesional_propuesta.blade.php
@@ -4,16 +4,25 @@
 
 @section('style')
 <link href="{{ url('css/perfil.css') }}" rel="stylesheet">
-<!-- <link href="{{ url('css/ofertas.css') }}" rel="stylesheet"> -->
+<link href="{{ url('css/table.css') }}" rel="stylesheet">
 @stop
 
 @section('content')
-	<h1>Mi perfil</h1>
-	<hr class="hr">
+<div class="contenedor"><!--inicio contenedor-->
+	<div class="div-1"><!--inicio div-1-->
+		<!-- <p>Mi Perfil</p> -->
+		<h1>Mi perfil</h1>
+		<hr class="hr">
+	</div><!--fin div-1-->
+	
 	<div class="clearfix">
-		<aside class="column" id="cssmenu">
-			@include('partials.aside')
-		</aside>
+		
+		<div class="div-2"><!--inicio div-2-->
+			<div class="div-2-1"><!--inicio div-2-1-->
+			<aside id="cssmenu" class="column hrV">
+				@include('partials.aside')
+			</aside>
+		</div><!--fin div-2-1-->
 		
 		<div class="column content">	
 			<form method="POST" action="#">			
@@ -467,7 +476,7 @@
 		</div>
 		
 	</div>
-
+</div><!--fin contenedor-->
 @stop
 
 {{-- Crear base de datos de imagenes, 20 para train, 50 para test, reconocer y detectar rostro --}}
