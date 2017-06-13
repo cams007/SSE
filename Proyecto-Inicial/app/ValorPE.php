3ed8db96b292d75cdefe45c95a01cc80cb5082fc
Adrian <adri.castillo18@gmail.com>
2017-06-13 18:45:56 -0500

    Se crea model and migration para ValorPE

---
+++ Proyecto-Inicial/app/ValorPE.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ValorPE extends Model
+{
+    protected $table = 'ValorPE';
+}
---
+++ Proyecto-Inicial/database/migrations/2017_06_13_234356_create_valor_p_es_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateValorPEsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ValorPE', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('catalogoValor_descripcion', 45);
+            $table->foreign('primerEmpleo_id')->references('id')->on('PrimerEmpleo');
+            $table->foreign('catalogoValor_id')->references('id')->on('CatalogoValor');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ValorPE');
+    }
+}
