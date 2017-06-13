6c07ac8edc94feb1577283d2e6ddcd1e68395000
Adrian <adri.castillo18@gmail.com>
2017-06-13 18:43:37 -0500

    Se crea model and migration para HabilidadPE

---
+++ Proyecto-Inicial/database/migrations/2017_06_13_234034_create_habilidad_pe_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHabilidadPEsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('HabilidadPE', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('catalogoHabilidad_descripcion', 45);
+            $table->foreign('primerEmpleo_id')->references('id')->on('PrimerEmpleo');
+            $table->foreign('catalogoHabilidad_id')->references('id')->on('CatalogoHabilidad');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('HabilidadPE');
+    }
+}
