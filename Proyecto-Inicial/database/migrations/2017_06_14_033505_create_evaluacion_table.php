307a92ab8dfa8ff29232d83ac23266084b3bcaa2
Adrian <adri.castillo18@gmail.com>
2017-06-13 22:36:37 -0500

    Se crea model and migration para Evaluacion

---
+++ Proyecto-Inicial/database/migrations/2017_06_14_033505_create_evaluacion_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Evaluacion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('evaluacion');
+            $table->foreign('empleado_id')->references('id')->on('Empleado');
+            $table->foreign('catalogoPregunta_id')->references('id')->on('CatalogoPregunta');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Evaluacion');
+    }
+}
