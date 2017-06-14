35e1ee9aecc03765e21a8f93767d2737b4a7ab7c
Adrian <adri.castillo18@gmail.com>
2017-06-13 22:24:55 -0500

    Se crea model and migration para Contacto

---
+++ Proyecto-Inicial/database/migrations/2017_06_14_032121_create_contacto_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Contacto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 90);
+            $table->string('puesto', 200);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Contacto');
+    }
+}
