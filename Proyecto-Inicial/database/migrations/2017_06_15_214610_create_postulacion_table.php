515eb4618bd331df15c6c04021e5a21394161100
Adrian <adri.castillo18@gmail.com>
2017-06-16 15:23:49 -0500

    Se agrega model and migration para Postulacion

---
+++ Proyecto-Inicial/database/migrations/2017_06_15_214610_create_postulacion_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostulacionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Postulacion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->varchar('egresado_matricula', 12);
+            $table->foreign('egresado_matricula')->references('matricula')->on('Egresado');
+            $table->integer('oferta_id');
+            $table->foreign('oferta_id')->references('id')->on('Oferta');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyContrains();
+        Schema::dropIfExists('Postulacion');
+        Schema::enableForeignKeyContrains();
+    }
+}
