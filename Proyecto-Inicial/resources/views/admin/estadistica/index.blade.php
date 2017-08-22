15891e1c9c8c8695441c8916b3b9111f6744be86
Lorenzo <lor90td@gmail.com>
2017-08-22 16:48:50 -0500

    Cambios al directorio local

--- Proyecto-Inicial/resources/views/admin/estadistica/index.blade.php
+++ Proyecto-Inicial/resources/views/admin/estadistica/index.blade.php
@@ -14,9 +14,48 @@
 			<p class="text-center">&Iacute;ndices y Estad&iacute;sticas</p>
 		</div>
 
-		<!-- Titulo de la consulta -->
-		<div class="div-1">
-			<p class="text-center">T&iacute;tulo de la consulta</p>
+<!--
+		{!! Form::select('carrera', config('options.carreras'), null, ['class' => 'seleccion', "onchange" => "this.form.submit()"]) !!}
+		{!! Form::close() !!}
+
+-->
+		<label class="label-egresados" id="label-egresados">Egresados</label>
+		<div class="opciones-egresados" id="opciones-egresados">		
+			<div>
+				{!! Form::select('select-egresado', [
+				's1' => 'G&eacute;nero de los encuestados',
+				's2' => 'Egresados con empleo',
+				's3' => 'Tiempo hasta conseguir primer empleo',
+				's4' => 'Dificultades para conseguir empleo',
+				's5' => 'Tipo de organismo donde laboran',
+				's6' => 'Tiempo de dedicaci&oacute;n al trabajo',
+				's7' => 'Tipo de formaci&oacute;n profesional',
+				's8' => 'Salario de los empleados',
+				's9' => 'Trato en donde estudiaron',
+				's10' => 'Satisfacci&oacute;n en la formaci&oacute;n',
+				's11' => 'Opini&oacute;n',
+				's12' => 'Titulados y no titulados',
+				's13' => 'Tiempo para obtener el t&iacute;tulo'] ) !!}
+
+				{!! Form::close() !!}
+			</div>
+		</div>
+
+		<label class="label-empleadores" id="label-empleadores">Empleadores</label>
+		<div class="opciones-empleadores" id="opciones-empleadores">
+			<div>
+				{!! Form::select('select-empleadores', [
+				's1' => 'Carreras m&aacute;s demandadas',
+				's2' => 'Opini&oacute;n sobre la formaci&oacute;n',
+				's3' => 'Opini&oacute;n sobre el desempeño laboral',
+				's4' => 'Importancia del t&iacute;tulo para contratar',
+				's5' => 'Importancia de la experiencia laboral',
+				's6' => 'Importancia de la imagen de la universidad',
+				's7' => 'Confianza de la contrataci&oacute;n de egresados de la universidad'],
+				null, [ 'class' => 'select-empleadores', "onchange" => "this.form.submit" ] ) !!}
+
+				{!! Form::close() !!}
+			</div>
 		</div>
 
 		<!-- Contenedor de grafica -->
@@ -26,7 +65,7 @@
 
 	<!-- Sript para mostrar el grafico -->
 	<?php
-
+	/*
 		// Instancia de un Lavacharts
 		$lava = new \Khill\Lavacharts\Lavacharts;
 		$stocksTable = $lava->DataTable();
@@ -43,5 +82,6 @@
 		// $chart = Lava::LineChart() if using Laravel
 		// echo"<div id='stocks-chart'></div>";
 		echo $lava->render('ColumnChart', 'MyStocks', 'stocks-chart');
+	*/
 	?>
 @stop
